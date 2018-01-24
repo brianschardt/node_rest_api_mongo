4196d23c3d149b70eeb68aab163a64d6005de4c5
brian schardt <brian.schardt@parcelpending.com>
2018-01-24 10:25:13 -0800

    changed config

--- config/config.js
+++ config/config.js
@@ -5,9 +5,9 @@ CONFIG = {} //Make this global to use all over the application
 CONFIG.app          = process.env.APP   || 'development';
 CONFIG.port         = process.env.PORT  || '3000';
 
-CONFIG.db_dialect   = process.env.DB_DIALECT    || 'mysql';
+CONFIG.db_dialect   = process.env.DB_DIALECT    || 'mongo';
 CONFIG.db_host      = process.env.DB_HOST       || 'localhost';
-CONFIG.db_port      = process.env.DB_PORT       || '3306';
+CONFIG.db_port      = process.env.DB_PORT       || '27017';
 CONFIG.db_name      = process.env.DB_NAME       || 'name';
 CONFIG.db_user      = process.env.DB_USER       || 'root';
 CONFIG.db_password  = process.env.DB_PASSWORD   || 'db-password';
